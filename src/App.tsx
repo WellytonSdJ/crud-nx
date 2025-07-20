9a6ddcd60987ce8a636dfb8cc45cce1d8b4f6cd6
Well <wellytonsdj@gmail.com>
2025-07-20 02:03:41 -0300

    feat: criado roteirizacao basica

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
-import { Home } from '@pages';
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Home } from '@pages/home';
+// import { Dashboard } from '@pages/dashboard';
 
-function App() {
-  return <Home/>
+const App: React.FC = () => {
+  return (
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* <Route path="/dashboard" element={<Dashboard />} /> */}
+          <Route path="*" element={<h2>Página não encontrada</h2>} />
+        </Routes>
+      </Router>
+  )
 }
 
 export default App
