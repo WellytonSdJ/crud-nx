17af9e95b2ae07d00e3d40e7dfa333e4e4d6bf27
Well <wellytonsdj@gmail.com>
2025-07-20 04:39:03 -0300

    feat: adicionado navbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 import Home from '@pages/home';
 import Dashboard from '@pages/dashboard';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from '@components/header';
 
 const App: React.FC = () => {
   return (
       <Router>
+        <Header/>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/dashboard" element={<Dashboard />} />
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import { Navbar, Container, Nav } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
 
-type Props = {}
-
-const Header = (props: Props) => {
+const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <Navbar bg="primary" variant="dark" expand="lg">
+      <Container>
+        <Navbar.Brand as={NavLink} to="/">
+          Meu App
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbar-nav" />
+        <Navbar.Collapse id="navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link as={NavLink} to="/" end>
+              Início
+            </Nav.Link>
+            <Nav.Link as={NavLink} to="/dashboard">
+              Dashboard
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
 
-export default Header
+export default Header;
